2dc13b659f93ebd8f24f9ff98dd10e2b10d2f218
dennis.muchiri1@student.moringaschool.com <dennis.muchiri1@student.moringaschool.com>
2025-05-20 14:59:11 +0300

    complete

--- lib/versions.py
+++ lib/versions.py
@@ -3,10 +3,10 @@ import requests
 import pytest
 
 def python_version():
-    return sys.version_info
+    return sys.version_info  # Returns something like (3, 8, 13, 'final', 0)
 
 def requests_version():
-    return requests.__version__
+    return requests.__version__  # Returns a string like '2.27.1'
 
 def pytest_version():
-    return pytest.__version__
+    return pytest.__version__  # Returns a string like '7.1.3'
